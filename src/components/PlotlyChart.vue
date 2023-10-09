4ca78599a35e8832cd6272434d60a75f83610657
easternnl <35781226+easternnl@users.noreply.github.com>
2023-10-09 08:26:49 +0200

    PlotlyChart component

---
+++ src/components/PlotlyChart.vue
@@ -0,0 +1,47 @@
+<script>
+/*
+  This Vue component
+
+ */
+
+import {v4 as uuidv4} from 'uuid';
+
+export default {
+  name: "PlotlyChart",
+  props: {
+    chartdetails: {
+      type: Object,
+      default: () => ({})
+    },
+
+  },
+  watch: {
+    chartdetails: function () {
+      this.drawChart()
+    }
+  },
+  computed: {
+    chartid: function () {
+      return uuidv4()
+    }
+  },
+  methods: {
+    drawChart: function () {
+      this.$plotly.newPlot(document.getElementById(this.chartid), this.chartdetails["data"], this.chartdetails["layout"], this.chartdetails["config"])
+    }
+  },
+  mounted: async function () {
+    this.drawChart()
+  }
+}
+</script>
+
+<template>
+  <div class="row">
+    <div v-bind:id="chartid"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
