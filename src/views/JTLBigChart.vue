0df9bd02f2cc254d9f495b6c87e0aa287705fcf1
easternnl <35781226+easternnl@users.noreply.github.com>
2023-10-10 13:11:36 +0200

    Update JTLBigChart to work faster

--- src/views/JTLBigChart.vue
+++ src/views/JTLBigChart.vue
@@ -1,35 +1,61 @@
 <script>
 import GlobalVariables from "@/GlobalVariableHolder";
+import PlotlyChart from "@/components/PlotlyChart.vue";
 
 export default {
   name: "JTLBigChart",
+  components: {PlotlyChart},
   computed: {
     normaltransactions: function () {
       return [...new Set(GlobalVariables.variables.$jtldata.map(item => item.label).filter((data) => !data.startsWith('#')))];
     },
-  },
-  async mounted() {
-    var alltransactions = []
+    responsetimes: function () {
 
-    this.normaltransactions.forEach(function (label) {
-      // walkthrough all transactions
-      if (label.charAt(0) != '#') {
+      var config = {responsive: true}
 
-        console.log('Adding ' + label)
+      var data = []
 
-        var x = GlobalVariables.variables.$jtldata.filter(function (item) {
-          if (item["label"] == label) return item
+      var reduceddata = this.$d3.rollup (GlobalVariables.variables.$jtldata.filter ((item) => {
+            if (this.normaltransactions.includes(item.label)) return item
+          }),
+          (v,e) => {
+            return {
+              avg: this.$d3.mean (v,
+                  e => e.elapsed),
+              max: this.$d3.max (v,
+                  e => e.elapsed),
+              min: this.$d3.min (v,
+                  e => e.elapsed),
+              pct90: this.$d3.quantile (v,
+                  0.90,
+                  e => e.elapsed),
+              pct95: this.$d3.quantile (v,
+                  0.95,
+                  e => e.elapsed),
+              pct99: this.$d3.quantile (v,
+                  0.99,
+                  e => e.elapsed),
+              count: this.$d3.count (v,
+                  e => e.elapsed),
+            }
+          },
+           d => d.label, d => parseInt (d.timeStamp / 10000))
 
-        }).map(function (item) {
-          return item["timeStamp"];
-        })
+      console.log('Rollup map:')
+      console.log(reduceddata)
 
-        var y = GlobalVariables.variables.$jtldata.filter(function (item) {
-          if (item["label"] == label) return item
-        }).map(function (item) {
-          return item["elapsed"] / 1000;
-        })
+      this.normaltransactions.forEach(function (label) {
+        console.log('Adding label ' + label)
 
+        var x = Array.from (reduceddata.get(label).keys ())
+            .map ((item) => {
+              return item * 10000
+            })
+
+        var y = Array.from (reduceddata.get(label).values ())
+            .map ((item) => {
+              return item["avg"] / 1000
+            })
 
         var transactiondetails = {
           type: 'line',
@@ -37,65 +63,164 @@ export default {
           x: x,
           y: y,
           mode: 'markers',
-          hoverlabel:
-              {
-                namelength: -1,
-                align: 'auto'
-              }
+          hoverlabel: {
+            namelength: -1,
+            align: 'auto'
+          },
+          visible: 'yes', // (['avg','min','max'].includes (label) ? 'yes' : 'legendonly'),
+          marker: {
+            // color: '#456E88',
+            symbol: (['max','pct90','pct95','pct99'].includes (label) ? 'triangle-up-open' : (['min'].includes (label) ? 'triangle-down-open' : 'open')),
+
+          }
         }
-        console.log(transactiondetails)
-        alltransactions.push(transactiondetails)
 
+        data.push (transactiondetails)
+
+      })
+
+      var layout = {
+        title: '', // this.transaction,
+        autosize: true,
+        height: window.innerHeight * 0.7,
+        hovermode: 'closest',
+        margin: {
+          l: 50,
+          r: 10,
+          t: 50,
+          b: 50,
+          pad: 10,
+          autoexpand: true
+        },
+        xaxis: {
+          //tickformat: '1',
+          title: 'Time',
+          type: 'date',
+          zeroline: true,
+          nticks: 20
+        },
+        yaxis: {
+          tickformat: '',
+          tick0: '0.0',
+          title: 'seconds',
+          rangemode: 'tozero'
+        },
+        legend: {
+          orientation: 'v',
+          traceorder: 'normal'
+        }
       }
 
-    })
-
-    var layout = {
-      //title:'Responsetimes for ' + this.transaction,
-      autosize: true,
-      height: window.innerHeight * 0.7,
-      hovermode: 'closest',
-      margin: {
-        l: 50,
-        r: 10,
-        t: 50,
-        b: 50,
-        pad: 10,
-        autoexpand: true
-      },
-      xaxis: {
-        //tickformat: '1',
-        title: 'Time',
-        type: 'date',
-        zeroline: true,
-        nticks: 20
-      },
-      yaxis: {
-        tickformat: '',
-        tick0: '0.1',
-        title: 'seconds',
-        rangemode: 'nonnegative'
-      },
-      legend: {
-        orientation: 'v',
-        traceorder: 'normal'
-
+      var retourobject = {
+        data: data,
+        layout: layout,
+        config: config
       }
+
+      console.log ("retourobject: ")
+      console.log (retourobject)
+      return retourobject
+
+    },
+    tps: function() {
+      var config = {responsive: true}
+
+      var data = []
+
+      var reduceddata = this.$d3.rollup (GlobalVariables.variables.$jtldata.filter ((item) => {
+            if (this.normaltransactions.includes(item.label)) return item
+        }),
+        (v,e) => {
+          return {
+            tps: this.$d3.count (v,
+                e => e.elapsed),
+          }
+        }, d => parseInt (d.timeStamp / 10000))
+
+
+      var x = Array.from (reduceddata.keys ())
+          .map ((item) => {
+            return item * 10000
+          })
+
+      var y = Array.from (reduceddata.values ())
+          .map ((item) => {
+            return item["tps"] / 10
+          })
+
+      var transactiondetails = {
+        type: 'line',
+        name: 'TPS of all transactions',
+        x: x,
+        y: y,
+        mode: 'lines',
+        hoverlabel: {
+          namelength: -1,
+          align: 'auto'
+        },
+        visible: 'yes',
+        marker: {
+          symbol: 'open',
+        }
+      }
+
+      data.push (transactiondetails)
+
+      var layout = {
+        title: 'TPS of all transactions',
+        autosize: true,
+        height: window.innerHeight * 0.7,
+        hovermode: 'closest',
+        margin: {
+          l: 50,
+          r: 10,
+          t: 50,
+          b: 50,
+          pad: 10,
+          autoexpand: true
+        },
+        xaxis: {
+          //tickformat: '1',
+          title: 'Time',
+          type: 'date',
+          zeroline: true,
+          nticks: 20
+        },
+        yaxis: {
+          tickformat: '',
+          tick0: '0.0',
+          title: 'transactions per second',
+          rangemode: 'tozero'
+        },
+        legend: {
+          orientation: 'v',
+          traceorder: 'normal'
+        }
+      }
+
+      var retourobject = {
+        data: data,
+        layout: layout,
+        config: config
+      }
+
+      console.log ("retourobject: ")
+      console.log (retourobject)
+      return retourobject
 
     }
+  },
+  async mounted() {
 
-    var config = {responsive: true}
-
-    this.$plotly.newPlot(document.getElementById("chart"), alltransactions, layout, config)
   }
 }
 
 </script>
 
 <template>
-  <h1>Grant chart</h1>
-
-  <div id="chart"></div>
+  <h1>Overview chart of all transactions</h1>
+  <PlotlyChart :chartdetails="responsetimes"></PlotlyChart>
+  <PlotlyChart :chartdetails="tps"></PlotlyChart>
 
 </template>
 
