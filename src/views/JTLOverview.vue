9470fae85155b0c6a451f064f01a2d9a751b5375
easternnl <35781226+easternnl@users.noreply.github.com>
2023-10-10 13:11:57 +0200

    Create overview table with percentiles

--- src/views/JTLOverview.vue
+++ src/views/JTLOverview.vue
@@ -2,16 +2,67 @@
 
 import { inject } from 'vue'
 import GlobalVariables from "@/GlobalVariableHolder";
+import MyTable from "@/components/MyTable.vue";
 
 export default {
   name: 'JTLOverview',
+  components: {MyTable},
   computed: {
     normaltransactions: function () {
       return [...new Set(GlobalVariables.variables.$jtldata.map(item => item.label).filter((data) => !data.startsWith('#')))];
     },
     hiddentransactions: function () {
       return [...new Set(GlobalVariables.variables.$jtldata.map(item => item.label).filter((data) => data.startsWith('#')))];
+    },
+    alltransactionsoverviewtable: function() {
+      var reduceddata = this.$d3.rollup(GlobalVariables.variables.$jtldata.filter ((item) => {
+            if (this.normaltransactions.includes(item.label)) return item
+          }),
+          (v,e) => {
+            return {
+              avg: this.$d3.mean (v,
+                  e => e.elapsed),
+              max: this.$d3.max (v,
+                  e => e.elapsed),
+              min: this.$d3.min (v,
+                  e => e.elapsed),
+              pct90: this.$d3.quantile (v,
+                  0.90,
+                  e => e.elapsed),
+              pct95: this.$d3.quantile (v,
+                  0.95,
+                  e => e.elapsed),
+              pct99: this.$d3.quantile (v,
+                  0.99,
+                  e => e.elapsed),
+              count: this.$d3.count (v,
+                  e => e.elapsed),
+            }
+          },
+          d => d.label)
+
+      console.log(reduceddata)
+
+      var data = []
+
+      // reformat the output and the sequence of the labels to show in the table
+      Array.from (reduceddata.keys ()).forEach(function(label) {
+
+        data.push({
+          Label: label,
+          Average: Math.round(reduceddata.get(label)["avg"]  ) / 1000,
+          Min: Math.round(reduceddata.get(label)["min"]  ) / 1000,
+          Max: Math.round(reduceddata.get(label)["max"]  ) / 1000 ,
+          Percentile90: Math.round(reduceddata.get(label)["pct90"]  ) / 1000,
+          Percentile95: Math.round(reduceddata.get(label)["pct95"]  ) / 1000,
+          Percentile99: Math.round(reduceddata.get(label)["pct99"]  ) / 1000,
+          Count: Math.round(reduceddata.get(label)["count"]  ) ,
+        })
+      })
+
+      return data
     }
+
 
 
   }
@@ -21,10 +72,12 @@ export default {
 </script>
 
 <template>
-  <p>Hello</p>
+  <div class="row">
+    <h1>Overview</h1>
 
-    <p>Normal Transactions: {{ normaltransactions }}</p>
-  <p>Hidden Transactions: {{ hiddentransactions }}</p>
+    <my-table :tabledata="alltransactionsoverviewtable"></my-table>
+<!--    <my-table :tabledata="alltransactionsoverviewtable"></my-table>-->
+  </div>
 </template>
 
 <style scoped>
