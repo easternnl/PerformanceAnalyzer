3bbfec3b7d076321e8db44990977339cc27022b6
easternnl <35781226+easternnl@users.noreply.github.com>
2024-02-05 13:50:01 +0100

    Update JTLOpenFile.vue
    Many changes

--- src/views/JTLOpenFile.vue
+++ src/views/JTLOpenFile.vue
@@ -1,13 +1,54 @@
 <template>
+
+  <div class="row mx-auto" v-if="error">
+    <p class="text-danger">
+      {{ error }}
+    </p>
+  </div>
+
   <div class="row mx-auto">
-    <p>Open the JTL file</p>
-
-    Import response messages: <input type="checkbox" v-model="responsemessages" v-bind:value="true">
-    Import failure messages: <input type="checkbox" v-model="failuremessages" v-bind:value="true">
-    Import hidden transactions <input type="checkbox" v-model="hiddentransactions" v-bind:value="true">
-
-
     <input class="btn btn-primary" type="file" @change="readFile($event)">
+  </div>
+
+  <div class="row mx-auto">
+    <table>
+      <tr>
+        <td>
+          Import response messages:
+        </td>
+        <td>
+          <input type="checkbox" v-model="responsemessages" v-bind:value="true">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Import failure messages:
+        </td>
+        <td>
+          <input type="checkbox" v-model="failuremessages" v-bind:value="true">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Import hidden transactions:
+        </td>
+        <td>
+          <input type="checkbox" v-model="hiddentransactions" v-bind:value="true">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Import thread names
+        </td>
+        <td>
+          <input type="checkbox" v-model="threadnames" v-bind:value="true">
+        </td>
+      </tr>
+    </table>
+
+
+
+
   </div>
 </template>
 
@@ -21,7 +62,9 @@ export default {
     return {
       responsemessages: true,
       failuremessages: true,
-      hiddentransactions: true
+      hiddentransactions: true,
+      threadnames: true,
+      error: ''
     }
   },
   mounted() {
@@ -33,76 +76,96 @@ export default {
       this.$emit('update:modelValue', event.target.files[0]);
     },
     uploadFile(file) {
+
         return new Promise((resolve, reject) => {
-          console.log('Init upload file')
 
           let fr = new FileReader();
+
           fr.onload = x=> resolve(fr.result);
-          fr.readAsText(file);
 
+          fr.onerror = function() {
+            console.log("Error during loading file")
+            console.log(fr.error);
+          };
 
+          return fr.readAsText(file);
+          fr.read
 
-          console.log('Finished upload file')
         })
     },
     async readFile(input) {
-      console.log("Init readfile")
-      console.log(GlobalVariables.variables.$jtldata)
 
       this.$nprogress.start()
 
-      console.log("Loading lines")
-      var lines = await this.uploadFile(input.target.files[0]);
 
-      console.log("Lines loaded")
+      console.time("Uploading file")
 
-      await this.$nprogress.inc(0.4)
+      console.log(input.target.files[0])
 
-      console.log("Parsing data")
+      var lines = await this.uploadFile(input.target.files[0]); // not sure if this construction is making the double amount of memory althoug it does not help with changing this construction
 
-      GlobalVariables.variables.$jtldata = this.$d3.csvParse(lines, (d) => {
-        if ((!d.label.startsWith('#')) || (d.label.startsWith('#') && this.hiddentransactions ) ) { // import hidden transaction
-          return {
-            timeStamp: +d.timeStamp,    // convert to number
-            elapsed: +d.elapsed,
-            label: d.label,
-            responseCode: +d.responseCode,
-            responseMessage: (this.responsemessages) ? d.responseMessage : '',
-            threadName: d.threadName,
-            dataType: d.dataType,
-            success: d.success,
-            failureMessage: (this.failuremessages) ? d.failureMessage : '',
-            bytes: +d.bytes,
-            sentBytes: +d.sentBytes,
-            grpThreads: +d.grpThreads,
-            allThreads: +d.allThreads,
-            URL: d.URL,
-            Latency: +d.Latency,
-            IdleTime: +d.IdleTime,
-            Connect: +d.Connect
+      console.timeEnd('Uploading file')
 
-          };
-        }
 
-      });
-      console.log("Parsing done")
-      await this.$nprogress.inc(0.7)
+      console.log('Imported lines: ' + lines.length)
 
-      console.log("Calculating meta data)")
+      if (!lines.length == 0) {
 
-      GlobalVariables.variables.$jtlinfo["min"] = this.$d3.min (GlobalVariables.variables.$jtldata,e => e.timeStamp)
-      GlobalVariables.variables.$jtlinfo["max"] = this.$d3.max (GlobalVariables.variables.$jtldata,e => e.timeStamp)
-      GlobalVariables.variables.$jtlinfo["duration"] = GlobalVariables.variables.$jtlinfo["max"] - GlobalVariables.variables.$jtlinfo["min"]
-      GlobalVariables.variables.$jtlinfo["filename"] = input.target.files[0]
+        await this.$nprogress.inc(0.4)
 
-      console.log("Calculating meta data done)")
+        console.log("Parsing data")
+        console.time('Parsing csv data to d3 object')
 
-      console.log(GlobalVariables.variables.$jtldata)
-      console.log('Readfile finished')
+        GlobalVariables.variables.$jtldata = this.$d3.csvParse(lines, (d) => {  // await this.uploadFile(input.target.files[0]); is possible as well
+          if ((!d.label.startsWith('#')) || (d.label.startsWith('#') && this.hiddentransactions)) { // import hidden transaction
+            return {
+              timeStamp: +d.timeStamp,    // convert to number
+              elapsed: +d.elapsed,
+              label: d.label,
+              responseCode: +d.responseCode,
+              responseMessage: (this.responsemessages) ? d.responseMessage : '',
+              threadName: (this.threadnames) ? d.threadName : '',
+              dataType: d.dataType,
+              success: d.success,
+              failureMessage: (this.failuremessages) ? d.failureMessage : '',
+              bytes: +d.bytes,
+              sentBytes: +d.sentBytes,
+              grpThreads: +d.grpThreads,
+              allThreads: +d.allThreads,
+              URL: d.URL,
+              Latency: +d.Latency,
+              IdleTime: +d.IdleTime,
+              Connect: +d.Connect
 
-      await this.$nprogress.inc(0.8)
+            };
+          }
 
-      this.$router.push('/jtloverviewnormal')
+        });
+        console.timeEnd('Parsing csv data to d3 object')
+
+        await this.$nprogress.inc(0.7)
+
+        console.time('Calculating meta data')
+
+        GlobalVariables.variables.$jtlinfo["min"] = this.$d3.min(GlobalVariables.variables.$jtldata, e => e.timeStamp)
+        GlobalVariables.variables.$jtlinfo["max"] = this.$d3.max(GlobalVariables.variables.$jtldata, e => e.timeStamp)
+        GlobalVariables.variables.$jtlinfo["duration"] = GlobalVariables.variables.$jtlinfo["max"] - GlobalVariables.variables.$jtlinfo["min"]
+        GlobalVariables.variables.$jtlinfo["filename"] = input.target.files[0].name
+
+        console.timeEnd('Calculating meta data')
+
+        console.log(GlobalVariables.variables.$jtldata)
+
+        await this.$nprogress.inc(0.8)
+
+        this.$router.push('/jtloverviewnormal')
+      }
+      else
+      {
+        this.error = "Failed to load file " + input.target.files[0].name + '<br> This is possible a memory limit in the browser. Try reducing the dataset with unclicking some options'
+
+        await this.$nprogress.done()
+      }
     }
 
   },
